2ee7be94226cd5d73255259fbf344ba39da4cbf7
Christian Justin <christian.justin23@gmail.com>
2024-04-20 02:37:26 +0700

    feat : Improve speeed dikit :')

--- src/links.go
+++ src/links.go
@@ -2,8 +2,10 @@ package main
 
 import (
     "fmt"
+    "log"
     "net/http"
     "strings"
+    "sync"
 
     "github.com/PuerkitoBio/goquery"
 )
@@ -14,8 +16,10 @@ type Node struct {
     Depth int
 }
 
+var httpClient = &http.Client{}
+
 func fetchPageLinks(pageName string) ([]string, error) {
-    res, err := http.Get("https://en.wikipedia.org/wiki/" + pageName)
+    res, err := httpClient.Get("https://en.wikipedia.org/wiki/" + pageName)
     if err != nil {
         return nil, err
     }
@@ -39,9 +43,13 @@ func fetchPageLinks(pageName string) ([]string, error) {
 }
 
 var linkCache = make(map[string][]string)
+var cacheMutex = &sync.Mutex{}
 
 func fetchPageLinksCached(pageName string) ([]string, error) {
-    if links, ok := linkCache[pageName]; ok {
+    cacheMutex.Lock()
+    links, ok := linkCache[pageName]
+    cacheMutex.Unlock()
+    if ok {
         return links, nil
     }
 
@@ -50,7 +58,9 @@ func fetchPageLinksCached(pageName string) ([]string, error) {
         return nil, err
     }
 
+    cacheMutex.Lock()
     linkCache[pageName] = links
+    cacheMutex.Unlock()
     return links, nil
 }
 
@@ -62,14 +72,16 @@ func DFS(start, goal string, maxDepth int) []string {
         node := stack[len(stack)-1]
         stack = stack[:len(stack)-1]
 
-
-
         if node.Name == goal {
             return node.Path
         }
 
         if node.Depth < maxDepth {
-            links, _ := fetchPageLinksCached(node.Name)
+            links, err := fetchPageLinksCached(node.Name)
+            if err != nil {
+                log.Printf("Failed to fetch links for page %s: %v", node.Name, err)
+                continue
+            }
             for _, link := range links {
                 if !visited[link] {
                     visited[link] = true
@@ -83,7 +95,7 @@ func DFS(start, goal string, maxDepth int) []string {
 }
 
 func main() {
-    path := DFS("Basketball", "Joko_Widodo", 2)
+    path := DFS("Mike_Tyson", "Joko_Widodo", 3)
     if path == nil {
         fmt.Println("No path found")
     } else {
