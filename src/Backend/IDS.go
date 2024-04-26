5378fefca211c0b05bcc894cc1c5b922ed7ffe41
Christian Justin <christian.justin23@gmail.com>
2024-04-27 05:39:53 +0700

    Nambahin komentar sama function all solution

--- src/Backend/IDS.go
+++ src/Backend/IDS.go
@@ -6,20 +6,23 @@ import (
 	"github.com/gocolly/colly"
     "container/list"
 	"net/url"
+	"sync"
 )
 
+// Mendefiniskan struktur Cache
 type Cache struct {
     maxEntries int
     ll         *list.List
     cache      map[interface{}]*list.Element
 }
 
+// Mendefiniskan struktur entry
 type entry struct {
     key   interface{}
     value interface{}
 }
 
-// Cache Related
+// Membuat instansi baru dari Cache
 func New(maxEntries int) *Cache {
     return &Cache{
         maxEntries: maxEntries,
@@ -28,6 +31,7 @@ func New(maxEntries int) *Cache {
     }
 }
 
+// Menambahkan data ke dalam cache
 func (c *Cache) Add(key, value interface{}) {
     if c.cache == nil {
         c.cache = make(map[interface{}]*list.Element)
@@ -45,6 +49,7 @@ func (c *Cache) Add(key, value interface{}) {
     }
 }
 
+//  Mendapatkan data dari cache
 func (c *Cache) Get(key interface{}) (value interface{}, ok bool) {
     if c.cache == nil {
         return
@@ -56,6 +61,7 @@ func (c *Cache) Get(key interface{}) (value interface{}, ok bool) {
     return
 }
 
+// Menghapus data terlama dari cache
 func (c *Cache) RemoveOldest() {
     if c.cache == nil {
         return
@@ -66,6 +72,7 @@ func (c *Cache) RemoveOldest() {
     }
 }
 
+// Menghapus elemen dari cache
 func (c *Cache) removeElement(e *list.Element) {
     c.ll.Remove(e)
     kv := e.Value.(*entry)
@@ -73,25 +80,47 @@ func (c *Cache) removeElement(e *list.Element) {
 }
 
 
+// Program Utama
 
+// Fungsi extractPageNameAndLang digunakan untuk mengekstrak nama halaman dan bahasa dari URL Wikipedia
+func extractPageNameAndLang(pageURL string) (string, string, error) {
+    u, err := url.Parse(pageURL)
+    if err != nil {
+        return "", "", err
+    }
+    lang := strings.Split(u.Host, ".")[0]
+    return strings.TrimPrefix(u.Path, "/wiki/"), lang, nil
+}
+
+// Fungsi fetchPageLinks digunakan untuk mengambil semua tautan halaman dari halaman Wikipedia tertentu
 func fetchPageLinks(pageName string, lang string) (map[string]struct{}, error) {
+    
+    // Membuat instance baru dari Collector menggunakan library colly
     c := colly.NewCollector()
 
+    // Membuat map untuk menyimpan tautan yang ditemukan
     links := make(map[string]struct{})
 
+    // Mengatur fungsi callback yang akan dipanggil ketika elemen HTML dengan selector "a[href]" ditemukan
     c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+        // Mengambil nilai dari atribut href
         link := e.Attr("href")
+        // Jika tautan tidak dimulai dengan "/wiki/", kita abaikan
         if !strings.HasPrefix(link, "/wiki/") {
             return
         }
+        // Jika tautan mengandung ":" atau "Category:", kita abaikan
         if strings.Contains(link, ":") || strings.Contains(link, "Category:") {
             return
         }
+        // Menghapus prefix "/wiki/" dari tautan dan menyimpannya di map
         pageTitle := strings.TrimPrefix(link, "/wiki/")
         links[pageTitle] = struct{}{}
     })
 
+    // Mengunjungi halaman Wikipedia dan memulai proses scraping
     err := c.Visit("https://" + lang + ".wikipedia.org/wiki/" + pageName)
+    
     if err != nil {
         return nil, err
     }
@@ -99,23 +128,33 @@ func fetchPageLinks(pageName string, lang string) (map[string]struct{}, error) {
     return links, nil
 }
 
-var linkCache = New(10000);
+// Membuat cache untuk menyimpan tautan halaman
+var linkCache = New(5000);
 
+
+// Fungsi ini menggunakan cache untuk menghindari pengambilan data yang sama berulang kali
 func fetchPageLinksCached(pageName string, lang string) (map[string]struct{}, error) {
+    // Mencoba mendapatkan tautan dari cache menggunakan nama halaman sebagai kunci
     links, ok := linkCache.Get(pageName)
+    // Jika tautan ditemukan di cache, kembalikan tautan tersebut dan nil sebagai error
     if ok {
         return links.(map[string]struct{}), nil
     }
 
+    // Jika tautan tidak ditemukan di cache, ambil tautan dari halaman Wikipedia menggunakan fungsi fetchPageLinks
     links, err := fetchPageLinks(pageName, lang)
+    
     if err != nil {
         return nil, err
     }
 
+    // Menambahkan tautan yang baru diambil ke cache untuk penggunaan di masa mendatang
     linkCache.Add(pageName, links)
+    
     return links.(map[string]struct{}), nil
 }
 
+// Node merepresentasikan simpul dalam graf pencarian
 type Node struct {
     Name     string
     Path     []string
@@ -123,47 +162,184 @@ type Node struct {
     Children []string
 }
 
+// Mendefinisikan struktur job untuk menyimpan informasi pekerjaan
 type job struct {
     pageName string
     lang     string
 }
 
+// Fungsi workers digunakan untuk memproses pekerjaan dari channel jobs dan mengirim hasilnya ke channel results
 func workers(id int, jobs <-chan job, results chan<- *Node) {
     for j := range jobs {
+		// Loop ini akan berjalan selama ada pekerjaan di channel jobs
         links, err := fetchPageLinksCached(j.pageName, j.lang)
         if err != nil {
             log.Printf("Failed to fetch links for page %s: %v", j.pageName, err)
             continue
         }
+
+		// Membuat slice untuk menyimpan tautan yang ditemukan
         children := make([]string, 0, len(links))
+
+		// Menambahkan tautan ke slice children
         for link := range links {
             children = append(children, link)
         }
+
+		// Mengirim Node baru ke channel results
         results <- &Node{j.pageName, nil, 0, children}
     }
 }
 
-
 var articlesChecked int
 
+// Fungsi DFS (Depth-First Search) digunakan untuk mencari jalur dari node start ke node goal
+// dengan kedalaman maksimum maxDepth. Fungsi ini menggunakan map visited untuk melacak node yang sudah dikunjungi
 func DFS(start, goal string, maxDepth int, visited map[string]bool, startLang, goalLang string) *Node {
+    // Membuat stack dan memasukkan node start ke dalam stack
+    stack := []*Node{{start, []string{start}, 0, nil}}
+
+    // Membuat channel untuk pekerjaan dan hasil
+    jobs := make(chan job, 100)
+    results := make(chan *Node, 100)
+
+    // Membuat WaitGroup
+    var wg sync.WaitGroup
+
+    // Memulai sejumlah goroutine pekerja
+    for w := 1; w <= ; w++ {
+        wg.Add(1)
+        go func(w int) {
+            defer wg.Done()
+            workers(w, jobs, results)
+        }(w)
+    }
+
+    // Membuat goroutine terpisah untuk menutup channel hasil setelah semua pekerjaan selesai
+    go func() {
+        wg.Wait()
+        close(results)
+    }()
+
+    // Melakukan pencarian DFS
+    for len(stack) > 0 {
+        // Mengambil node terakhir dari stack
+        node := stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+
+        // Jika node adalah goal, kembalikan node tersebut
+        if node.Name == goal {
+            return node
+        }
+
+        // Jika kedalaman node kurang dari maxDepth
+        if node.Depth < maxDepth {
+            // Jika node tidak memiliki anak, kirim nama halaman ke channel pekerjaan
+            if node.Children == nil {
+                jobs <- job{node.Name, startLang}
+                // Terima hasil dari channel hasil
+                newNode := <-results
+                node.Children = newNode.Children
+            }
+
+            // Untuk setiap tautan di anak node
+            for _, link := range node.Children {
+                // Jika tautan belum dikunjungi
+                if !visited[link] {
+                    visited[link] = true
+                    articlesChecked++
+                    // Membuat slice baru untuk menyimpan path
+                    newPath := make([]string, len(node.Path))
+                    // Menyalin path node saat ini ke slice baru
+                    copy(newPath, node.Path)
+                    // Menambahkan tautan ke path baru
+                    newPath = append(newPath, link)
+                    // Membuat node baru dan menambahkannya ke stack
+                    newNode := &Node{link, newPath, node.Depth + 1, nil}
+                    stack = append(stack, newNode)
+                }
+            }
+            // Mengosongkan anak node
+            node.Children = node.Children[:0] 
+        }
+    }
+
+    // Menutup channel pekerjaan setelah semua pekerjaan telah dikirim
+    close(jobs)
+
+    // Jika tidak ada jalur yang ditemukan, kembalikan nil
+    return nil
+}
+
+
+// Fungsi IDS (Iterative Deepening Search) digunakan untuk mencari jalur dari URL start ke URL goal
+// dengan kedalaman maksimum maxDepth. Fungsi ini mengembalikan jalur dalam bentuk slice string
+func IDS(startURL, goalURL string, maxDepth int) []string {
+    // Mengekstrak nama halaman dan bahasa dari URL start
+    start, startLang, err := extractPageNameAndLang(startURL)
+    // Jika terjadi error, cetak pesan error dan kembalikan nil
+    if err != nil {
+        log.Printf("Failed to extract page name from URL %s: %v", startURL, err)
+        return nil
+    }
+
+    // Mengekstrak nama halaman dan bahasa dari URL goal
+    goal, goalLang, err := extractPageNameAndLang(goalURL)
+    // Jika terjadi error, cetak pesan error dan kembalikan nil
+    if err != nil {
+        log.Printf("Failed to extract page name from URL %s: %v", goalURL, err)
+        return nil
+    }
+
+    // Melakukan pencarian IDS
+    for depth := 0; depth <= maxDepth; depth++ {
+        // Membuat map untuk melacak halaman yang sudah dikunjungi
+        visited := make(map[string]bool)
+        visited[start] = true
+        // Melakukan pencarian DFS dengan kedalaman tertentu
+        node := DFS(start, goal, depth, visited, startLang, goalLang)
+        // Jika jalur ditemukan, kembalikan jalur tersebut
+        if node != nil {
+            return node.Path
+        }
+    }
+    // Jika tidak ada jalur yang ditemukan, kembalikan nil
+    return nil
+}
+
+// Bonus
+func DFSMany(start, goal string, maxDepth int, visited map[string]bool, startLang, goalLang string, paths chan<- []string) {
     stack := []*Node{{start, []string{start}, 0, nil}}
 
     // Create a channel for jobs and a channel for results
     jobs := make(chan job, 100)
     results := make(chan *Node, 100)
 
+    // Create a WaitGroup
+    var wg sync.WaitGroup
+
     // Start a fixed number of worker goroutines
     for w := 1; w <= 10; w++ {
-        go workers(w, jobs, results)
+        wg.Add(1)
+        go func(w int) {
+            defer wg.Done()
+            workers(w, jobs, results)
+        }(w)
     }
+
+    // Create a separate goroutine to close the results channel after all jobs are done
+    go func() {
+        wg.Wait()
+        close(results)
+    }()
 
     for len(stack) > 0 {
         node := stack[len(stack)-1]
         stack = stack[:len(stack)-1]
 
         if node.Name == goal {
-            return node
+            paths <- node.Path
+            continue
         }
 
         if node.Depth < maxDepth {
@@ -190,11 +366,10 @@ func DFS(start, goal string, maxDepth int, visited map[string]bool, startLang, g
         }
     }
 
-    return nil
+    close(jobs) // Close the jobs channel after all jobs have been sent
 }
 
-
-func IDS(startURL, goalURL string, maxDepth int) []string {
+func IDSMany(startURL, goalURL string, maxDepth int) [][]string {
     start, startLang, err := extractPageNameAndLang(startURL)
     if err != nil {
         log.Printf("Failed to extract page name from URL %s: %v", startURL, err)
@@ -207,27 +382,23 @@ func IDS(startURL, goalURL string, maxDepth int) []string {
         return nil
     }
 
+    paths := make(chan []string, 10)
     for depth := 0; depth <= maxDepth; depth++ {
         visited := make(map[string]bool)
         visited[start] = true
-        node := DFS(start, goal, depth, visited, startLang, goalLang)
-        if node != nil {
-            return node.Path
+        go DFSMany(start, goal, depth, visited, startLang, goalLang, paths)
+    }
+
+    var solutions [][]string
+    for path := range paths {
+        solutions = append(solutions, path)
+        if len(solutions) >= 10 {
+            break
         }
     }
-    return nil
+
+    return solutions
 }
-
-func extractPageNameAndLang(pageURL string) (string, string, error) {
-    u, err := url.Parse(pageURL)
-    if err != nil {
-        return "", "", err
-    }
-    lang := strings.Split(u.Host, ".")[0]
-    return strings.TrimPrefix(u.Path, "/wiki/"), lang, nil
-}
-
-
 
 // func main() {
 //     start := time.Now()
